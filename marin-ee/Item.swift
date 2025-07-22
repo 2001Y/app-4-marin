60242473febe17f386d5de26a68d0073ddcf0fe7
2001Y <15167896+2001Y@users.noreply.github.com>
2025-07-22 12:15:44 +0900

    Initial Commit

---
+++ marin-ee/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  marin-ee
+//
+//  Created by 2001Y on 2025/07/22.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
